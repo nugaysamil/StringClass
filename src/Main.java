f94bdb6093c1160defd7c8af2ba72540b4397ea8
Şamil Nugay <samil_nugay@hotmail.com>
2022-07-30 01:03:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+    public static void main(String[ ]args) {
+
+
+        //System.out.println(b);
+
+        //System.out.println("Harf Sayisi : " + b.length()); // b harfinin lenght'i
+
+        //System.out.println("0. indexs " + b.charAt(0)); // 0 inndeksi eleman
+        //System.out.println("Son Eleman :  " + b.charAt(b.length()-1)); // son eleman
+
+        //for (int i = 0 ; i< b.length(); i++) {
+          //  System.out.println(b.charAt(i));
+
+        /*System.out.println(b.startsWith("As"));// baslangıc true or false
+        System.out.println(b.endsWith("Co")); // son true or false */
+
+        //System.out.println(b.indexOf('a')); // 'a' harfi hangi indexde olduğunu gösterir
+
+       // System.out.println(b.lastIndexOf('a')); // sondan başlayarak referans'ı tanır.
+
+        //System.out.println(b.toLowerCase()); // değerdekileri kücük harfe çevirir.
+        //System.out.println(b.toUpperCase()); // büyük harfe çevirir
+
+        /*String a1 = "1923";
+
+        int b1 = Integer.parseInt(a1); // Int to String
+        System.out.println(b1 - 42);*/
+
+       // int b1 = 1923;
+
+        //String a = String.valueOf(b1); // String to int
+        //System.out.println(b1);
+
+        String a1 = "Mustafa";
+        String a2 = "Mustafa";
+
+        /*if( a1 == a2 ) {
+
+            System.out.println("Esittler");*/
+        }
+
+        //String b = new String("Mustafa");//
+
+       String b1 = new String("Mustafa");
+       String b2 = new String("Mustafa");
+
+      if(b1 == b2) {
+        System.out.println("Esitler");
+    }
+
+
+
+
+
+    }
